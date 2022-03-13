6bc3859c398305a2915d59ff04732025a33c743d
Kristaps Naglis <krnaglis@gmail.com>
2022-03-13 23:08:32 +0200

    update todo list

--- main.cpp
+++ main.cpp
@@ -10,26 +10,25 @@
 // lettersum("microspectrophotometries") => 317
 
 // === Additional Challenges ===
-// ! Use the enable1 word list for the optional bonus challenges.
-// 1) microspectrophotometries is the only word with a letter sum of 317. Find the only word with a letter sum of 319.
+// ! Use the word list for the optional bonus challenges.
+// TODO 1) microspectrophotometries is the only word with a letter sum of 317. Find the only word with a letter sum of 319.
 // ---
-// 2) How many words have an odd letter sum?
+// TODO 2) How many words have an odd letter sum?
 // ---
-// 3) There are 1921 words with a letter sum of 100, making it the second most common letter sum. What letter sum is
+// TODO 3) There are 1921 words with a letter sum of 100, making it the second most common letter sum. What letter sum is
 // most common, and how many words have it?
 // ---
-// 4) zyzzyva and biodegradabilities have the same letter sum as each other (151), and their lengths differ by 11
+// TODO 4) zyzzyva and biodegradabilities have the same letter sum as each other (151), and their lengths differ by 11
 // letters. Find the other pair of words with the same letter sum whose lengths differ by 11 letters.
 // ---
-// 5) cytotoxicity and unreservedness have the same letter sum as each other (188), and they have no letters in common.
+// TODO 5) cytotoxicity and unreservedness have the same letter sum as each other (188), and they have no letters in common.
 // Find a pair of words that have no letters in common, and that have the same letter sum, which is larger than 188.
 // (There are two such pairs, and one word appears in both pairs.)
 // ---
-// 6) The list of word { geographically, eavesdropper, woodworker, oxymorons } contains 4 words. Each word in the list
+// TODO 6) The list of word { geographically, eavesdropper, woodworker, oxymorons } contains 4 words. Each word in the list
 // has both a different number of letters, and a different letter sum. The list is sorted both in descending order of
 // word length, and ascending order of letter sum. What's the longest such list you can find?
 
-// TODO - Additional challenges
 
 #include <iostream>
 #include <vector>
