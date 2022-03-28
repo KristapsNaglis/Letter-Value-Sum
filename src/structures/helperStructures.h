2532f7af224bfae83748822395d2f1e2280e21be
Kristaps Naglis <krnaglis@gmail.com>
2022-03-28 21:12:51 +0300

    add helper header for custom structures

---
+++ src/structures/helperStructures.h
@@ -0,0 +1,14 @@
+//
+// Created by kristapsn on 24.03.22.
+//
+#include <string>
+
+#ifndef LETTER_VALUE_SUM_TWOWORDSONESUM_H
+#define LETTER_VALUE_SUM_TWOWORDSONESUM_H
+
+struct twoWordsOneSum {
+    std::pair<std::string, std::string> words;
+    unsigned int sum;
+};
+
+#endif //LETTER_VALUE_SUM_TWOWORDSONESUM_H
